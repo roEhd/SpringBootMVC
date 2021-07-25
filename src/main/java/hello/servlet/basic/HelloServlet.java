e499555c70a29723b253e2b7d87fed4329e12434
roehd <mekieun@ewhain.net>
2021-07-25 13:25:42 +0900

    Initial commit

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        //response의 헤더에 들어가는 내용
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+
+        //response의 바디에 들어가는 내용
+        response.getWriter().write("hello " + username);
+    }
+}
